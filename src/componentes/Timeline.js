66d903b8c6c101fb70ed41e876297e90001d3cc2
Jorge Guimaraes <jorgeguimaraes@cinbesa.com.br>
2019-03-22 12:01:05 -0300

    alteração na timeline

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import FotoItem from './FotoItem';
 
+
 export default class Timeline extends Component {
-    render(){
-        return (
-        <div className="fotos container">
-          <FotoItem/>
-          <FotoItem/>
-        </div>            
-        );
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      fotos : []
     }
+  }
+
+
+  componentDidMount()
+  {
+
+    fetch("http://localhost:8080/api/public/fotos/rafael")
+    .then(response => response.json())
+    .then(fotos => {
+      this.setState({fotos : fotos});
+    })
+
+  }
+
+
+  render(){
+      return (
+      <div className="fotos container">
+
+        {this.state.fotos.map(foto => {
+          return <FotoItem  foto = {foto} />          
+        })}
+        
+      </div>            
+      );
+  }
 }
